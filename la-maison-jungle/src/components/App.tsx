91d89bc80ffc2dfb362b2eed02b531d4eb79bd63
Julien <jujufrancois1999@gmail.com>
2022-04-06 23:09:01 +0200

    Add state in different components

--- la-maison-jungle/src/components/App.tsx
+++ la-maison-jungle/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "./Banner.tsx";
 import logo from '../img/leaf+1.png'
 import Cart from "./Cart.tsx";
@@ -7,16 +7,17 @@ import Footer from "./Footer.tsx";
 import '../assets/Layout.css'
 
 function App() {
+	const [cart, updateCart] = useState([]);
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
-			</div>
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
 			<Footer />
 		</div>
 	)
